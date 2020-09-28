34e6bf7b6f35b0f879a6f7818035a4c163162ea5
ivand890 <ivand890@gmail.com>
2020-09-27 22:11:07 -0500

    fix percent error

--- main.py
+++ main.py
@@ -43,14 +43,19 @@ tmode_import = (
 tmode_total = (tmode_import + tmode_export).sort_values(ascending=False)
 
 # Opción tres
-# Calculo de los las rutas de importación y exportación que representan 
+# Calculo por países de las ganancias en  importación y exportación que representan 
 # el valor mas cercano al 80 % de las ganancias totales. Cálculo del 
 # porcentaje real que representan esa fracción de rutas
-eighty_export = export_total[export_total.cumsum() <= export_total.sum() * 4 / 5]
-actual_export_percent = round(eighty_export.sum() / export_total.sum() * 100, 1)
 
-eighty_import = import_total[import_total.cumsum() <= import_total.sum() * 4 / 5]
-actual_import_percent = round(eighty_import.sum() / import_total.sum() * 100, 1)
+export_ori = export_data.groupby(["origin"]).sum()["total_value"].sort_values(ascending=False)
+
+eighty_export = export_ori[export_ori.cumsum() <= (export_ori.sum() * 4 / 5 + 5*10**9)]
+actual_export_percent = round(eighty_export.sum() / export_ori.sum() * 100, 1)
+
+
+import_dest = import_data.groupby(["destination"]).sum()["total_value"].sort_values(ascending=False)
+eighty_import = import_dest[(import_dest.cumsum() <= import_dest.sum() * 4 / 5 + 3*10**9)]
+actual_import_percent = round(eighty_import.sum() / import_dest.sum() * 100, 1)
 
 
 # Guardado de las la información ya procesada en distintos archivos csv 
@@ -60,6 +65,7 @@ export_total.to_csv("export_total.csv")
 import_total.to_csv("import_total.csv")
 tmode_export.to_csv("tmode_export.csv")
 tmode_import.to_csv("tmode_import.csv")
+tmode_total.to_csv("tmode_total.csv")
 eighty_export.to_csv("eighty_export.csv")
 eighty_import.to_csv("eighty_import.csv")
 
